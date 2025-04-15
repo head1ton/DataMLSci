507bff0e9508a98b21a9ee5c3c560c3f617ff536
hwan <head1ton@gmail.com>
2025-04-15 21:51:03 +0900

    basic stock complete

---
+++ finance/finance.py
@@ -0,0 +1,23 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Stock Price App
+
+Shown are the stock **closing price** and ***volume*** of META!
+""")
+
+#define the ticker symbol
+tickerSymbol = 'META'
+# tickerSymbol = 'GOOG'
+# tickerSymbol = 'AAPL'
+
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2015-3-31', end='2025-3-31')
+# Open  High    Low Close   Volume  Dividends   Stock Splits
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
