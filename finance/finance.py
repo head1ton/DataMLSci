cd640e8d838aaaf3912faa7d0f3e5874807b1e70
hwan <head1ton@gmail.com>
2025-04-15 22:24:02 +0900

    finance

--- finance/finance.py
+++ finance/finance.py
@@ -19,5 +19,15 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2015-3-31', end='2025-3-31')
 # Open  High    Low Close   Volume  Dividends   Stock Splits
 
+st.write("""
+## Combined Price
+""")
+st.line_chart(tickerDf[['Close', 'Volume']])
+st.write("""
+## Closing Price
+""")
 st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
 st.line_chart(tickerDf.Volume)
