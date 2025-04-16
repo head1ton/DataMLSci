af4abcabc581e4af83062b8b8316aaa689c96aca
hwan <head1ton@gmail.com>
2025-04-17 04:07:59 +0900

    pomodoro & multi page

---
+++ pomodoro/pomodoro.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import time
+import os
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+def remote_css(url):
+    st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+css_path = os.path.join(base_dir, "style.css")
+local_css(css_path)
+
+st.write("""
+# The Pomodoro
+
+Let's do some focus work in data science with this app.
+
+""")
+
+button_clicked = st.button("START")
+
+t1 = 1500
+t2 = 300
+
+if button_clicked:
+    with st.empty():
+        while t1:
+            mins, secs = divmod(t1, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            st.header(f"⏳ {timer}")
+            time.sleep(1)
+            t1 -= 1
+            st.success("🔔 25 minutes is over! Time for a break!")
+
+    with st.empty():
+        while t2:
+            mins2, secs2 = divmod(t1, 60)
+            timer2 = '{:02d}:{:02d}'.format(mins2, secs2)
+            st.header(f"⏳ {timer2}")
+            time.sleep(1)
+            t2 -= 1
+            st.error("⏰ 5 minute break is over!")
